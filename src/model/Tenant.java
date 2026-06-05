dad91e301385a8a2064316feb3e98c7cd8e2d31a
Bisrat Zenebe <bisratzenebe94@gmail.com>
2026-06-05 19:12:47 +0300

    Updates on Apartment, Studio and Tenant classes

--- src/model/Tenant.java
+++ src/model/Tenant.java
@@ -10,7 +10,55 @@ public class Tenant {
     private List<RentalAgreement> rentalHistory;
 
     // constructor
-    // getters & setters
+    public Tenant(String tenantId, String name, String phone, String email){
+        this.tenantId = tenantId;
+        this.name = name;
+        this.phone = phone;
+        this.email = email;
+        this.rentalHistory = new ArrayList<>();
+    }
+    // getters
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    //setters
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
     // Methods
+    public void addRentalHistory(RentalAgreement rental){
+        rentalHistory.add(rental);
+    }
+    // Display Method
+    public void displayInfo(){
+        System.out.println("=== TENANT INFORMATION ===");
+        System.out.println("ID: " + tenantId);
+        System.out.println("Name: " + name);
+        System.out.println("Phone: " + phone);
+        System.out.println("Email: " + email);
+        System.out.println("Past Rentals: " + rentalHistory.size());
+    }
+    // File Storage Method
+    public String toFileString(){
+        return tenantId + "|" + name + "|" + phone + "|" + email;
+    }
 
 }
