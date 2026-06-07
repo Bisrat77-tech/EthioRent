7df61e4be905379ff8951f55cf79c933e8cb3a1e
Aya <ayaanwarmoh27@gmail.com>
2026-06-07 08:04:27 +0300

    Updated Property class

--- src/model/Property.java
+++ src/model/Property.java
@@ -10,5 +10,46 @@ public class Property {
     protected PropertyStatus status;
 
     // Constructor, getters
+    public Property (String propertyId, String address, double rent, int bedrooms, int bathrooms, PropertyStatus status) {
+        this.propertyId = propertyId;
+        this.address = address;
+        this.rent = rent;
+        this.bedrooms = bedrooms;
+        this.bathrooms = bathrooms;
+        this.status = status;
+    }
+
+    public String getPropertyId() {
+        return propertyId;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public double getRent() {
+        return rent;
+    }
+
+    public int getBedrooms() {
+        return bedrooms;
+    }
+
+    public int getBathrooms() {
+        return bathrooms;
+    }
+
+    public PropertyStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(PropertyStatus status) {
+        this.status = status;
+    }
+
     // methods
+    @Override
+    public String toString() {
+        return "Property ID: " + propertyId + ", Address: " + address + ", Rent: " + rent + ", Bedrooms: " + bedrooms + ", Bathrooms: " + bathrooms + ", Status: " + status;
+    }
 }
