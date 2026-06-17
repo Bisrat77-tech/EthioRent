3f0caf9dd76b72bd2236a0d4b3a66d16ef976546
Aya <ayaanwarmoh27@gmail.com>
2026-06-17 15:17:02 +0300

    Updated Studio and Tenant classes

--- src/model/Studio.java
+++ src/model/Studio.java
@@ -1,7 +1,37 @@
 package model;
 
-public class Studio {
-    private boolean isFurnished;
+public class Studio extends Property {
 
-    // constructor, override
+    private boolean furnished;
+
+    public Studio(String propertyId, String address,
+                  double rent, int bedrooms,
+                  int bathrooms, boolean furnished) {
+
+        super(propertyId, address, rent, bedrooms, bathrooms);
+        this.furnished = furnished;
+    }
+
+    public boolean isFurnished() {
+        return furnished;
+    }
+
+    @Override
+    public void displayInfo() {
+
+        System.out.println("---- STUDIO ----");
+        System.out.println("ID: " + propertyId);
+        System.out.println("Address: " + address);
+        System.out.println("Monthly Rent: " + rent + " ETB");
+        System.out.println("Bedrooms: " + bedrooms);
+        System.out.println("Bathrooms: " + bathrooms);
+        System.out.println("Status: " + status);
+        System.out.println("Furnished: " +
+                (furnished ? "Yes" : "No"));
+    }
+
+    @Override
+    public String getType() {
+        return "Studio";
+    }
 }
--- src/model/Tenant.java
+++ src/model/Tenant.java
@@ -1,16 +1,60 @@
 package model;
 
-import java.util.*;
-
 public class Tenant {
+
     private String tenantId;
-    private String name;
-    private String phone;
+    private String fullName;
+    private String phoneNumber;
     private String email;
-    private List<RentalAgreement> rentalHistory;
 
-    // constructor
-    // getters & setters
-    // Methods
+    public Tenant(String tenantId, String fullName,
+                  String phoneNumber, String email) {
 
+        this.tenantId = tenantId;
+        this.fullName = fullName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void displayInfo() {
+        System.out.println("---- TENANT ----");
+        System.out.println("ID: " + tenantId);
+        System.out.println("Name: " + fullName);
+        System.out.println("Phone: " + phoneNumber);
+        System.out.println("Email: " + email);
+    }
+
+    public String toFileString() {
+        return tenantId + "|" + fullName + "|" +
+                phoneNumber + "|" + email;
+    }
+
+    @Override
+    public String toString() {
+        return toFileString();
+    }
 }
