d0656473f0b1f22520cda009c218aed10a439122
Aya <ayaanwarmoh27@gmail.com>
2026-06-18 10:24:24 +0300

    Updated Rental Agreement class

--- src/model/RentalAgreement.java
+++ src/model/RentalAgreement.java
@@ -3,6 +3,7 @@ package model;
 import java.util.*;
 
 public class RentalAgreement {
+
     private String rentalId;
     private Property property;
     private Tenant tenant;
@@ -12,5 +13,64 @@ public class RentalAgreement {
     private boolean isActive;
     private List<Double> paymentHistory;
 
-    // constructor , getters ,Methods
+    public RentalAgreement(String rentalId,
+                           Property property,
+                           Tenant tenant,
+                           Date startDate,
+                           Date endDate,
+                           double deposit) {
+
+        this.rentalId = rentalId;
+        this.property = property;
+        this.tenant = tenant;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.deposit = deposit;
+        this.isActive = true;
+        this.paymentHistory = new ArrayList<>();
+
+        property.setStatus(PropertyStatus.RENTED);
+    }
+
+    public void recordPayment(double amount) {
+        paymentHistory.add(amount);
+    }
+
+    public double getTotalPayments() {
+        double total = 0;
+        for (double p : paymentHistory) {
+            total += p;
+        }
+        return total;
+    }
+
+    public void terminateAgreement() {
+        isActive = false;
+        property.setStatus(PropertyStatus.AVAILABLE);
+    }
+
+    public String getRentalId() {
+        return rentalId;
+    }
+
+    public Property getProperty() {
+        return property;
+    }
+
+    public Tenant getTenant() {
+        return tenant;
+    }
+
+    public void displayInfo() {
+
+        System.out.println("---- RENTAL AGREEMENT ----");
+        System.out.println("Rental ID: " + rentalId);
+        System.out.println("Property: " + property.getPropertyId());
+        System.out.println("Tenant: " + tenant.getFullName());
+        System.out.println("Deposit: " + deposit);
+        System.out.println("Start: " + startDate);
+        System.out.println("End: " + endDate);
+        System.out.println("Active: " + isActive);
+        System.out.println("Total Payments: " + getTotalPayments());
+    }
 }
