e0cb1a2bc86486d96d40033c8da9b514337a167b
Christtina <christinasolomon072@gmail.com>
2026-06-19 16:06:43 +0300

    Refactor House class

--- src/model/House.java
+++ src/model/House.java
@@ -1,16 +1,16 @@
 package model;
 
 public class House extends Property {
-// House fields
+
     private double lotSize;
     private boolean hasGarden;
-// Constructor
+
     public House(String propertyId, String address, double rent, int bedrooms, int bathrooms, double lotSize, boolean hasGarden){
         super(propertyId, address, rent, bedrooms, bathrooms);
         this.lotSize = lotSize;
         this.hasGarden = hasGarden;
     }
-// Overriding abstract method from parent class
+
     @Override
     public void displayInfo(){
         System.out.println("=== HOUSE PROPERTY ===");
@@ -26,7 +26,7 @@ public class House extends Property {
     protected String getType() {
         return "House";
     }
-// Getters
+
 
     public double getLotSize() {
         return lotSize;
